d48bd1dcf10bda5ca0bd868885cba2c54457fcfd
bmedx <bmesick@edx.org>
2019-04-12 11:54:48 -0400

    Re-adding django-simple-history for CourseEnrollment

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1301,6 +1301,9 @@ MIDDLEWARE_CLASSES = [
 
     'edx_rest_framework_extensions.auth.jwt.middleware.EnsureJWTAuthSettingsMiddleware',
 
+    # Handles automatically storing user ids in django-simple-history tables when possible.
+    'simple_history.middleware.HistoryRequestMiddleware',
+
     # This must be last
     'openedx.core.djangoapps.site_configuration.middleware.SessionCookieDomainOverrideMiddleware',
 ]
