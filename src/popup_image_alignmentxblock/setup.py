daef66fb6bec1a7622a8c20bdf9a82c5ac70678f
virajut <virajthakrar@gmail.com>
2019-09-24 08:23:18 +0000

    Adding pop_image_alignment xblock after ignoring src folder from tracking

---
+++ src/popup_image_alignmentxblock/setup.py
@@ -0,0 +1,43 @@
+"""Setup for popup_image_alignmentxblock XBlock."""
+
+from __future__ import absolute_import
+
+import os
+
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='popup_image_alignmentxblock-xblock',
+    version='0.1',
+    description='popup_image_alignmentxblock XBlock',   # TODO: write a better description.
+    license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
+    packages=[
+        'popup_image_alignmentxblock',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'popup_image_alignmentxblock = popup_image_alignmentxblock:PopupImageAlignmentXBlock',
+        ]
+    },
+    package_data=package_data("popup_image_alignmentxblock", ["static", "public"]),
+)
