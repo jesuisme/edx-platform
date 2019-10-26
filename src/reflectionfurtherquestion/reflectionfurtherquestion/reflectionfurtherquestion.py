93970b0fddc2ccee9e64aae148d2960939466c86
nitin-vaishwade <nitin.vaishwade@luminad.com>
2019-10-26 15:07:48 +0530

    reflection question xblock

---
+++ src/reflectionfurtherquestion/reflectionfurtherquestion/reflectionfurtherquestion.py
@@ -0,0 +1,149 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+import pkg_resources
+from xblock.core import XBlock
+from xblock.fields import Integer, Scope, List, String
+from xblock.fragment import Fragment
+
+@XBlock.wants('user')
+class ReflectionFurtherXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    # TO-DO: delete count, and define your own fields.
+    studio_questions = String(
+        default='Add Questions?',display_name='Questions', scope=Scope.content,
+        help="A simple counter, to show something happening",
+    )
+
+    question = String(help="A question for students", default="testing questions?", scope=Scope.content)
+    responses = List(help="responses from students", default=[], scope=Scope.user_state_summary)
+    
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the QuestionresponseXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/reflectionfurtherquestion.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/reflectionfurtherquestion.css"))
+        frag.add_javascript(self.resource_string("static/js/src/reflectionfurtherquestion.js"))
+        frag.initialize_js('ReflectionFurtherXBlock')
+        return frag
+
+    def studio_view(self, context=None):
+        """
+        This is the view that renders the XBlock in the Studio editor, allowing the instructor to configure it.
+        """
+
+
+        html = self.resource_string(
+            "static/html/edit_studio.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string(
+            "static/css/reflectionfurtherquestion.css"))
+        frag.add_javascript(self.resource_string("static/js/src/edit_studio.js"))
+        frag.initialize_js('ReflectionUserResponseXBlocks')
+        return frag
+
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    # @XBlock.json_handler
+    # def increment_count(self, data, suffix=''):
+    #     """
+    #     An example handler, which increments the data.
+    #     """
+    #     # Just to show data coming in...
+    #     assert data['hello'] == 'world'
+
+    #     self.count += 1
+    #     return {"count": self.count}
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @XBlock.json_handler
+    def set_question(self, data, suffix=''):
+        """
+        this handler accepts the question 
+        """
+
+        self.question = data['question']
+        return {"question": self.question}
+
+    @XBlock.json_handler
+    def set_studio_question(self, data, suffix=''):
+        """
+        this handler accepts the question 
+        """
+
+        self.studio_questions = data['studio_questions']
+
+        return {"studio_questions": self.studio_questions}
+
+    @XBlock.json_handler
+    def add_reply(self, data, suffix=''):
+        """
+        this handler accepts a new reply
+        """
+        user_service = self.runtime.service(self, 'user')
+        xb_user = user_service.get_current_user()
+        user_reply = data['studentReply']
+        # course_id = data['course_id']
+        newReply = {
+            "student": xb_user.full_name,
+            "email": xb_user.emails,
+            "reply": data['studentReply']
+        }
+        self.responses.append(newReply)
+        return {"responses": self.responses}
+
+
+    @XBlock.json_handler
+    def check_user_reply(self, data, suffix=''):
+        """
+        this handler accepts a new reply
+        """
+        user_service = self.runtime.service(self, 'user')
+        xb_user = user_service.get_current_user()
+        # course_id = data['course_id']
+        user_data = self.responses
+        user_match_counter = 0
+        if len(user_data) > 0:
+            for user_row in user_data:
+                if str(user_row['email'][0]) == str(xb_user.emails[0]):
+                    user_match_counter += 1
+
+        if user_match_counter > 0:
+            return {"responses": self.responses}
+        else:
+            return {"responses": "new_user"}
+
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("ReflectionFurtherXBlock",
+             """<reflectionfurtherquestion/>
+             """),
+            ("Multiple ReflectionFurtherXBlock",
+             """<vertical_demo>
+                <reflectionfurtherquestion/>
+                <reflectionfurtherquestion/>
+                <reflectionfurtherquestion/>
+                </vertical_demo>
+             """),
+        ]
---
+++ src/reflectionfurtherquestion/reflectionfurtherquestion/static/js/src/reflectionfurtherquestion.js
@@ -0,0 +1,205 @@
+/* Javascript for ReflectionFurtherXBlock. */
+function ReflectionFurtherXBlock(runtime, element) {
+
+    var addResponseUrl = runtime.handlerUrl(element, 'add_response');
+    var addReplyUrl = runtime.handlerUrl(element, 'add_reply');
+    var responseSlides = document.getElementById('responses');
+
+
+    $('#addresponse', element).click(function (eventObject) {
+        var response = document.getElementById("studentresponse").value;
+        var slide = "";
+        // var course_id;
+        // var current_url = window.location.href;
+        // var split_url = current_url.split("/");
+        // for (i = 0; i < split_url.length; i++) { 
+        //   if (split_url[i].includes("course-v1:")){
+        //     console.log(split_url[i]);
+        //     course_id = split_url[i];
+
+        //   }
+        // }
+        $.ajax({
+            type: "POST",
+            url: addReplyUrl,
+            data: JSON.stringify({
+                "studentReply": response,
+            }),
+            success: function (data) {
+                responseSlides.innerHTML = "";
+                data.responses.reverse();
+                var count = 0;
+                var j = 0;
+                for (var i = 0; i < data.responses.length; i++) {
+                    if (i === 0) {
+                        var active = 'active';
+                    } else {
+                        var active = '';
+                    }
+                    if (count === 0) {
+                        var html = '<div class="item ' + active + '">';
+                        $(".carousel-indicators").append('<li data-target="#responseCarousel" data-slide-to="' + j + '" class="' + active + '"></li>');
+                        j++;
+                    }
+                    if (data.responses[i].reply.length > 200) {
+                        var dot = "...";
+                        var readmore = "<p class='read-more' data-reply='"+data.responses[i].reply+"' onclick=\"$('#popup-content').text($(this).data('reply')); $('.popup_ques, #overlay').fadeIn();\"> Read More<p>";
+                    } else {
+                        var dot = "";
+                        var readmore = '';
+                    }
+
+                    
+                    html += `<div class="col-sm-4"
+                                style="box-sizing: border-box; position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 33.33%;">
+                                <div class="well text-left small"
+                                    style="box-sizing: border-box; font-size: 11.9px; text-align: left; min-height: 20px; padding: 19px; margin-bottom: 20px; background: none 0px 0px repeat scroll #ffffff; border: 0px solid #e3e3e3; border-radius: 0px; box-shadow: none; color: #8e9091;">
+                                    <div class="iHeight"
+                                        style="box-sizing: border-box; max-height: 155px; overflow: hidden; min-height: 155px !important;">
+                                        <span style="box-sizing: border-box;">`+ data.responses[i].reply.substring(0, 200) + dot + `</span>` + readmore + `</div>
+                                    <br/><span style="box-sizing: border-box;"><span
+                                            class="fa fa-user-circle fa-3x" aria-hidden="true"
+                                            style="box-sizing: border-box; display: inline-block; font-variant-numeric: normal; font-variant-east-asian: normal; font-stretch: normal; font-size: 3em; line-height: 1; font-family: FontAwesome; text-rendering: auto; -webkit-font-smoothing: antialiased;"></span>&nbsp;<span
+                                            class="text-uppercase"
+                                            style="box-sizing: border-box; text-transform: uppercase; color: #000000;">`+ data.responses[i].student + `</span></span><br style="box-sizing: border-box;" /><a
+                                        href="#" class="Org"
+                                        style="box-sizing: border-box; background-color: transparent; color: #f58320; text-decoration-line: none; cursor: pointer; line-height: 22px;">Lumina
+                                        Datamatics Ltd</a><br style="box-sizing: border-box;" /><a
+                                        href="#" class="PPRole"
+                                        style="box-sizing: border-box; background-color: transparent; color: #f58320; text-decoration-line: none; cursor: pointer; line-height: 22px;">Researcher</a>
+                                </div>
+                            </div>`;
+
+
+                    count++;
+                    if (count === 3 ) {
+                        html += '</div>';
+                        count = 0;
+                        slide += html;
+                    }else if((i+1) == data.responses.length && count != 3){
+                        html += '</div>';
+                        slide += html;
+                    }
+
+
+                }
+
+                responseSlides.innerHTML = slide;
+                $("#ask_question").hide();
+                $('#responseCarousel-container').show();
+            }
+        });
+    });
+
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+        //get the question on page load
+
+    var check_user_reply = runtime.handlerUrl(element, 'check_user_reply');
+    var slide = "";
+    // var course_id;
+    // var current_url = window.location.href;
+    // var split_url = current_url.split("/");
+    // for (i = 0; i < split_url.length; i++) { 
+    //   if (split_url[i].includes("course-v1:")){
+    //     console.log(split_url[i]);
+    //     course_id = split_url[i];
+
+    //   }
+    // }
+
+    $.ajax({
+            type: "POST",
+            url: check_user_reply,
+            data: JSON.stringify({
+                "course_id": "course_id"
+            }),
+            success: function (data) {
+                
+                if (data.responses != "new_user"){
+                    responseSlides.innerHTML = "";
+                    console.log("replies: ", data.responses);
+                    data.responses.reverse();
+                    var count = 0;
+                    var j = 0;
+                    for (var i = 0; i < data.responses.length; i++) {
+                        if (i === 0) {
+                            var active = 'active';
+                        } else {
+                            var active = '';
+                        }
+                        if (count === 0) {
+                            var html = '<div class="item ' + active + '">';
+                            $(".carousel-indicators").append('<li data-target="#responseCarousel" data-slide-to="' + j + '" class="' + active + '"></li>');
+                            j++;
+                        }
+                        if (data.responses[i].reply.length > 200) {
+                            var dot = "...";
+                            var readmore = "<p class='read-more' data-reply='"+data.responses[i].reply+"' onclick=\"$('#popup-content').text($(this).data('reply')); $('.popup_ques, #overlay').fadeIn();\"> Read More<p>";
+                        } else {
+                            var dot = "";
+                            var readmore = '';
+                        }
+
+                        
+                        html += `<div class="col-sm-4"
+                                    style="box-sizing: border-box; position: relative; min-height: 1px; padding-right: 15px; padding-left: 15px; float: left; width: 33.33%;">
+                                    <div class="well text-left small"
+                                        style="box-sizing: border-box; font-size: 11.9px; text-align: left; min-height: 20px; padding: 19px; margin-bottom: 20px; background: none 0px 0px repeat scroll #ffffff; border: 0px solid #e3e3e3; border-radius: 0px; box-shadow: none; color: #8e9091;">
+                                        <div class="iHeight"
+                                            style="box-sizing: border-box; max-height: 155px; overflow: hidden; min-height: 155px !important;">
+                                            <span style="box-sizing: border-box;">`+ data.responses[i].reply.substring(0, 200) + dot + `</span>` + readmore + `</div>
+                                        <br/><span style="box-sizing: border-box;"><span
+                                                class="fa fa-user-circle fa-3x" aria-hidden="true"
+                                                style="box-sizing: border-box; display: inline-block; font-variant-numeric: normal; font-variant-east-asian: normal; font-stretch: normal; font-size: 3em; line-height: 1; font-family: FontAwesome; text-rendering: auto; -webkit-font-smoothing: antialiased;"></span>&nbsp;<span
+                                                class="text-uppercase"
+                                                style="box-sizing: border-box; text-transform: uppercase; color: #000000;">`+ data.responses[i].student + `</span></span><br style="box-sizing: border-box;" /><a
+                                            href="#" class="Org"
+                                            style="box-sizing: border-box; background-color: transparent; color: #f58320; text-decoration-line: none; cursor: pointer; line-height: 22px;">Lumina
+                                            Datamatics Ltd</a><br style="box-sizing: border-box;" /><a
+                                            href="#" class="PPRole"
+                                            style="box-sizing: border-box; background-color: transparent; color: #f58320; text-decoration-line: none; cursor: pointer; line-height: 22px;">Researcher</a>
+                                    </div>
+                                </div>`;
+
+                        console.log(count);
+
+                        count++;
+                        if (count === 3 ) {
+                            html += '</div>';
+                            count = 0;
+                            slide += html;
+                        }else if((i+1) == data.responses.length && count != 3){
+                            html += '</div>';
+                            slide += html;
+                        }
+
+
+
+                    }
+
+                    responseSlides.innerHTML = slide;
+                    $("#ask_question").hide();
+                    $('#responseCarousel-container').show();
+                }
+
+            }
+                
+
+
+        });
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+}
